e938ea7e23a193ece6721eb75e5d08a65adaed7b
Andrey Khitryy <andrey.khitryy@gmail.com>
2016-07-27 18:43:29 +0300

    Cleanup sampo added

--- app/src/main/java/ga/arkty/caster/MainActivity.java
+++ app/src/main/java/ga/arkty/caster/MainActivity.java
@@ -32,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
         castManager = VideoCastManager.getInstance();
         castConsumer = new SingleVideoCastConsumer(this,
-                "https://goo.gl/E80S9M",
+                "http://storage14.swap.sampo.ru/%D0%A1%D0%B5%D1%80%D0%B8%D0%B0%D0%BB%D1%8B/%D0%97%D0%B0%D1%80%D1%83%D0%B1%D0%B5%D0%B6%D0%BD%D1%8B%D0%B5/%D0%98%D0%B3%D1%80%D0%B0%20%D0%BF%D1%80%D0%B5%D1%81%D1%82%D0%BE%D0%BB%D0%BE%D0%B2%20%28Game%20of%20Thrones%29/%D0%A1%D0%B5%D0%B7%D0%BE%D0%BD%206/Game.of.Thrones.S06E10.1080p.rus.LostFilm.TV.mkv",
                 "Jet Packs Was Yes", "Periphery",
-                "http://fugostudios.com/wp-content/uploads/2012/02/periphery720p-600x338.jpg", "video/mp4") {
+                "http://fugostudios.com/wp-content/uploads/2012/02/periphery720p-600x338.jpg", "video/mkv") {
             @Override
             public void onPlaybackFinished() {
                 disconnectDevice();
